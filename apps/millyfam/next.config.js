4af2c6e62b9ac711f3e0c5816c803834ab1b0183
DistractionBoy <jason.pierce.designs@gmail.com>
2024-12-14 12:32:50 -0500

    fix: adjust next.config to try to transpile shared-ui

--- apps/millyfam/next.config.js
+++ apps/millyfam/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@optichads-app/shared-ui"],
+  experimental: {
+    externalDir: true,
+  },
   eslint: {
     ignoreDuringBuilds: false,
     dirs: ["pages", "components", "lib", "public"],
